db7e046a918d76317b643a12e550d42506528088
Jonah Milner <jonahmilner86@gmail.com>
2022-02-19 13:32:58 -0700

    figured out I was doing everything wrong, even though I almost had it working, so I started over

--- Develop/script.js
+++ Develop/script.js
@@ -1,21 +1,7 @@
 // Timestamp
 let timeDay = moment().format('MMMM Do YYYY hh:mm');
-
 $('#currentDay').append(timeDay);
 
-let currentTime = moment().format("hh mm");
-
-let timeDisplay = parseInt($('.list-group-item'));
-
-if (currentTime >= timeDisplay) {
-   $('.list-group-item').css('background-color', 'red');
-}
-else if (currentTime <= timeDisplay) {
-    $('.list-group-item').css('background-color', 'gray');
-}
-else {
-    $('.list-group-item').css('background-color', 'green');
-}
 
 
 
