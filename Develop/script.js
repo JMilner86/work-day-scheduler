a809b022abc0df09f3ca83aac98409578ecc8f72
Jonah Milner <jonahmilner86@gmail.com>
2022-02-19 15:45:19 -0700

    didn't add anything, still trying to figure it out

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,30 @@
 // Timestamp
 let timeDay = moment().format('MMMM Do YYYY hh:mm');
+
 $('#currentDay').append(timeDay);
 
+
+
+let currentTime = moment().format("hh:mm");
+
+let timeDisplay = $('.hour');
+
+
+console.log(timeDisplay);
+
+
+// If the time is equal to the time displayed show red
+if (currentTime >= timeDisplay) {
+   $('.time-block row').css('.present');
+}
+// If time is past show gray
+else if (currentTime <= timeDisplay) {
+    $('.time-block row').css('.past');
+}
+// If time is yet to come show green
+else {
+    $('.list-group-item').css('.future');
+}
 
 
 
