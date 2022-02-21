7a81828f7d1e1ecad3ae734c12a1445407cde779
Jonah Milner <jonahmilner86@gmail.com>
2022-02-21 16:37:42 -0700

    finished the project added some comments

--- Develop/script.js
+++ Develop/script.js
@@ -2,27 +2,55 @@
 let timeDay = moment().format('MMMM Do YYYY hh:mm');
 $('#currentDay').append(timeDay);
 
-// moment().format("MM ddd, YYYY hh:mm:ss a")
-// Put the current time into a function
-// Dynamically declare the time-blocks?
-// Maybe put the time blocks into an array?
-// Add event listener on page load
-// $('.container').append()
-// 0:div.hour childnodes text data
-
-
-const getTime = (timeBlock) => {
-    let timeList = parseInt($(timeBlock).find('.hour').text().trim());
-    let currentTime = moment().format("hh:mm");
-    console.log(timeList)
-};
-
-setInterval(function() {
-    $(".time-block").each(function(index, el) {
-      getTime(el);
-    });
-}, 1000);
-
-if (timeList < currentTime) {
-    $('.time-block row').addClass('past')
+// Set the time blocks colors
+const getTime = () => {
+   let time = parseInt(moment().format('HH'));
+   $('.time-block').each(function(){
+       let hour = parseInt($(this).attr('id'));
+      if (hour < time) {
+    $(this).find('textarea').addClass('past')
 }
+
+else if (time == hour) {
+    $(this).find('textarea').addClass('present')
+}
+
+else {
+    $(this).find('textarea').addClass('future')
+} 
+   })
+
+};
+// Checks every second to see what time it is by running getTime every 30 minutes
+setInterval(
+ getTime(),   
+ 1800000);
+// save to local storage
+const saveInput = () => {
+    localStorage.setItem('9', $('#9').find('textarea').val());
+    localStorage.setItem('10', $('#10').find('textarea').val());
+    localStorage.setItem('11', $('#11').find('textarea').val());
+    localStorage.setItem('12', $('#12').find('textarea').val());
+    localStorage.setItem('13', $('#13').find('textarea').val());
+    localStorage.setItem('14', $('#14').find('textarea').val());
+    localStorage.setItem('15', $('#15').find('textarea').val());
+    localStorage.setItem('16', $('#16').find('textarea').val());
+    localStorage.setItem('17', $('#17').find('textarea').val());
+    loadInput();
+};
+// pull from local storage
+const loadInput = () => {
+   $('#9').find('textarea').val(localStorage.getItem('9'));
+   $('#10').find('textarea').val(localStorage.getItem('10'));
+   $('#11').find('textarea').val(localStorage.getItem('11'));
+   $('#12').find('textarea').val(localStorage.getItem('12'));
+   $('#13').find('textarea').val(localStorage.getItem('13'));
+   $('#14').find('textarea').val(localStorage.getItem('14'));
+   $('#15').find('textarea').val(localStorage.getItem('15'));
+   $('#16').find('textarea').val(localStorage.getItem('16'));
+   $('#17').find('textarea').val(localStorage.getItem('17'));
+}
+// run functions and add event listeners
+$('.saveBtn').on('click', saveInput);
+loadInput();
+
